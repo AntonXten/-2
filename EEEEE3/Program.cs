a2d71ae3ebef2ab89fb0125b3fc698dd8ee7fee4
Anton <antonxten@gmail.com>
2022-02-02 01:09:17 +0300

    Продолжем делать ДЗ

---
+++ EEEEE3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Выяснить является ли число чётным
+
+    
+    Console.WriteLine("Введите число: ");
+  
+
+  
+    {
+        Console.WriteLine("Число четное");
+    }
+    else
+    {
+        Console.WriteLine("Число нечетное");
+    }
+    
+    Console.Reeadkey();
+}
