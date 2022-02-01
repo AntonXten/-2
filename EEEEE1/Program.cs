1af6c62cdf061f9fdc26173e03e0b235dd3cc7d0
Anton <antonxten@gmail.com>
2022-02-02 00:44:37 +0300

    Ланч

---
+++ EEEEE1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Даны два числа. Показать большее и меньшее число
+int numA = 10;
+int numB = 100;
+
+if (numA == numB)
+{
+Console.WriteLine("Равны");
+}
+else if(numA>numB)
+{
+Console.WriteLine(numA);
+Console.WriteLine(numB);
+}
+else
+{
+Console.WriteLine(numB);
+Console.WriteLine(numA);
+}
